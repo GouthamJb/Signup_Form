a5c7b13f7ad34f70e99204ac14150a5d49b56fe5
GouthamJb <gouthamj@qburst.com>
2020-11-02 10:50:22 +0530

    Signup Form 1.HTML file was added. 2.Script was added. 3.css file was added.

---
+++ Scripts/myscript.js
@@ -0,0 +1,23 @@
+
+function myFunction(){
+    alert("Thank you for entering your details!");
+}
+$(document).ready(function(){
+    $(document).find(".vehicleselector").hide();
+    $("input[type='radio']").click(function(){
+        var radioValue = $("input[name='yes_no']:checked").val();
+        console.log(radioValue);
+        if(radioValue == 'yes')
+        {
+            $(document).find(".vehicleselector").show();
+            $(document).find("#vehicle").attr("required",true)
+        }
+        else if(radioValue == 'no')
+        {
+            $(document).find(".vehicleselector").hide();
+            $(document).find("#vehicle").attr("required",false)
+        }
+
+
+    });
+});
